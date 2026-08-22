65cc8041c2b0b39374f23ebe4e46557a74627389
hui18Q <CHIN.WEN.HUI@student.mmu.edu.my>
2026-08-22 20:01:56 +0800

    cv update

---
+++ pipeline/defect_classifier.py
@@ -0,0 +1,41 @@
+from ultralytics import YOLO
+
+# Load the trained model once
+model = YOLO("model/best.pt")
+
+
+def classify_defect(image_path):
+    """
+    Classify a thermal solar-panel image.
+
+    Input:
+        image_path:
+            Path to a thermal image.
+
+    Output:
+        Dictionary containing:
+        - predicted defect class
+        - confidence score
+    """
+
+    # Give the image to our trained YOLO model
+    results = model(image_path)
+
+    # We only gave YOLO one image,
+    # so take the first result.
+    result = results[0]
+
+    # Get the model's highest-probability class.
+    class_id = result.probs.top1
+
+    # Convert class number into class name.
+    class_name = result.names[class_id]
+
+    # Get confidence score.
+    confidence = float(result.probs.top1conf)
+
+    # Return structured information.
+    return {
+        "class": class_name,
+        "confidence": confidence,
+    }
